0b50ef84909cb65e12665d78b6b6c276c33cbd5a
Ashwini Bhide <60039504+slytherin007@users.noreply.github.com>
2023-12-30 12:49:52 +0530

    Add files via upload

---
+++ 30-12-23/05_const.php
@@ -0,0 +1,9 @@
+<?php
+// case-sensitive constant name
+define("GREETING", "Welcome to Mumbai!");
+echo GREETING;
+echo "<br>";
+// case-insensitive constant name
+define("GREETING", "Welcome to Mumbai!", true);
+echo greeting;
+?> 
